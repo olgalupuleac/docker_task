689784af9fe39faf61d31ffbef2d576abacbee9c
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-06-04 22:49:55 +0300

    Add sys args

--- receive.py
+++ receive.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import boto3
-
+import sys
 # Create SQS client
 sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/')
-queue1 = sqs.get_queue_by_name(QueueName='A')
-queue2 = sqs.get_queue_by_name(QueueName='B')
+
+name1, name2 = sys.argv[1], sys.argv[2]
+queue1 = sqs.get_queue_by_name(QueueName=name1)
+queue2 = sqs.get_queue_by_name(QueueName=name2)
 
 while True:
     for message in queue1.receive_messages():
--- send.py
+++ send.py
@@ -5,5 +5,4 @@ sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/')
 queue1 = sqs.create_queue(QueueName='A')
 queue2 = sqs.create_queue(QueueName='B')
 queue1.send_message(MessageBody='1')
-queue1.send_message(MessageBody='2')
 

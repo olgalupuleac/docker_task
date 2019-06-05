421de65a9eb282be310ea6997929d767b42463f3
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-06-06 01:33:59 +0300

    Add region_name

--- receive.py
+++ receive.py
@@ -2,7 +2,7 @@
 import boto3
 import sys
 # Create SQS client
-sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/')
+sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/', region_name='eu_west_1')
 
 name1, name2 = sys.argv[1], sys.argv[2]
 queue1 = sqs.get_queue_by_name(QueueName=name1)
--- send.py
+++ send.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import boto3
 
-sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/')
+sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/', region_name='eu_west_1')
 queue1 = sqs.create_queue(QueueName='A')
 queue2 = sqs.create_queue(QueueName='B')
 queue1.send_message(MessageBody='1')

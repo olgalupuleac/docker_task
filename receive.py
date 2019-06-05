a58cf5974ba81a65c905de112d73448a04a10dca
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-06-06 02:04:21 +0300

    Add credentials

--- receive.py
+++ receive.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import boto3
 import sys
-# Create SQS client
-sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/', region_name='eu_west_1')
+import time
+
+time.sleep(20)
+sqs = boto3.resource('sqs', endpoint_url='http://localstack:4576', region_name='eu_west_1',
+                     aws_access_key_id='ACCESS_KEY', aws_secret_access_key='SECRET_KEY')
 
 name1, name2 = sys.argv[1], sys.argv[2]
 queue1 = sqs.get_queue_by_name(QueueName=name1)
@@ -11,7 +14,7 @@ queue2 = sqs.get_queue_by_name(QueueName=name2)
 while True:
     for message in queue1.receive_messages():
         n = message.body
-        print(n)
-        queue2.send_message(MessageBody=str(int(n) + 1))
+        print(n, flush=True)
         message.delete()
+        queue2.send_message(MessageBody=str(int(n) + 1))
 
--- send.py
+++ send.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import boto3
+import time
 
-sqs = boto3.resource('sqs', endpoint_url='http://localhost:4576/', region_name='eu_west_1')
+time.sleep(10)
+
+sqs = boto3.resource('sqs', endpoint_url='http://localstack:4576', region_name='eu_west_1', aws_access_key_id='ACCESS_KEY', aws_secret_access_key='SECRET_KEY')
 queue1 = sqs.create_queue(QueueName='A')
 queue2 = sqs.create_queue(QueueName='B')
 queue1.send_message(MessageBody='1')
